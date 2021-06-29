3eae3da5aee228568082a2f018683ceb1a162b0a
Mohammed Mansour <shalia.kwt@outlook.com>
2021-06-29 20:13:22 +0300

    finished

---
+++ material-components-android-codelabs-102-starter/java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductCardViewHolder.java
@@ -0,0 +1,20 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+public class ProductCardViewHolder extends RecyclerView.ViewHolder {
+        public NetworkImageView productImage;
+        public TextView productTitle, productPrice;
+    public ProductCardViewHolder(@NonNull View itemView) {
+        super(itemView);
+        productTitle = itemView.findViewById(R.id.product_title);
+        productPrice = itemView.findViewById(R.id.product_price);
+        productImage = itemView.findViewById(R.id.product_image);
+    }
+}
